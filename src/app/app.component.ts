09f7542331d9fccd41c880f97dafe103fbb26f0a
Aditi Kulkarni <aditi.kulkarni@worldline.com>
2024-09-11 11:31:52 +0530

    Changes save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EmpListComponent} from "./emp-list/emp-list.component";
 import { CommonModule } from '@angular/common';
+import {EmployeedetailsComponent} from "./EmpManagement/employeedetails/employeedetails.component";
+import {DemoComponent} from "./demo/demo.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,EmpListComponent],
+  imports: [RouterOutlet,EmpListComponent,EmployeedetailsComponent,DemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
